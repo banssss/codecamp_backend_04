dec15f97bd5efa1cc8c2136afe59cfbe63ee2a45
banssss <watermelonzv@gmail.com>
2022-07-13 23:39:51 +0900

    change method - findOne, updateOne

--- homework/day08/index.js
+++ homework/day08/index.js
@@ -20,7 +20,7 @@ app.post('/tokens/phone', async (req, res) => {
     const mytoken = getToken();
 
     // 3. 폰 번호가 DB에 저장되어 있는지 확인 후 업데이트하기
-    const result = await Token.find({ phone:myphone });
+    const result = await Token.findOne({ phone:myphone });
     // 3-1-1. 폰 번호가 저장되어있지 않다면, 새로 할당하여 DB에 저장하기
     if(result.length === 0){
         const token = new Token({
@@ -30,7 +30,7 @@ app.post('/tokens/phone', async (req, res) => {
         });
         await token.save();
         // 3-1-2. 폰 번호에 토큰 전송하기
-        sendTokenToSMS(myphone, mytoken);
+        // sendTokenToSMS(myphone, mytoken);
         res.send(
             token.phone.slice(0,3)+'-'+
             token.phone.slice(3,7)+'-'+
@@ -38,14 +38,14 @@ app.post('/tokens/phone', async (req, res) => {
         );
     // 3-2-1. 폰 번호가 이미 저장되어있는 중복번호라면, token 업데이트하기
     } else {
-        result[0].token = mytoken;
-        await result[0].save();
+        result.token = mytoken;
+        await Token.updateOne({phone:myphone}, {$set:{token:mytoken}});
         // 3-2-2. 폰 번호에 토큰 전송하기
-        sendTokenToSMS(myphone, mytoken);
+        // sendTokenToSMS(myphone, mytoken);
         res.send(
-            result[0].phone.slice(0,3)+'-'+
-            result[0].phone.slice(3,7)+'-'+
-            result[0].phone.slice(7,11)+'으로 인증 문자가 전송되었습니다.'
+            result.phone.slice(0,3)+'-'+
+            result.phone.slice(3,7)+'-'+
+            result.phone.slice(7,11)+'으로 인증 문자가 전송되었습니다.'
         );
     }
 });
@@ -54,7 +54,7 @@ app.patch('/tokens/phone', async (req,res)=>{
     const myphone = req.body.phone;
 
     // 1. 입력받은 핸드폰 번호를 DB - Tokens 문서에서 찾아오기
-    const result = await Token.find({ phone:myphone });
+    const result = await Token.findOne({ phone:myphone });
 
     // 1-1. 핸드폰 번호가 저장되어있지 않다면, 클라이언트에 false를 응답하고 함수를 종료하기
     if(result.length === 0){
@@ -63,7 +63,7 @@ app.patch('/tokens/phone', async (req,res)=>{
     }
     // 2. 입력받은 토큰을 DB의 토큰정보와 비교하기.
     const myToken = req.body.token;
-    const dbToken = result[0].token;
+    const dbToken = result.token;
     // 2-1. 일치하지 않으면 클라이언트에 false를 응답하고 함수를 종료하기
     if(myToken !== dbToken){
         res.send(false);
@@ -71,8 +71,8 @@ app.patch('/tokens/phone', async (req,res)=>{
     }
     // 2-2. 일치하면 isAuth를 true로 변경하여 DB에 저장후 클라이언트에 true 응답하기.
     if(myToken === dbToken){
-        result[0].isAuth = true;
-        await result[0].save();
+        result.isAuth = true;
+        await result.updateOne({phone:myphone}, {$set:{isAuth:true}});
         res.send(true);
     }
 });
