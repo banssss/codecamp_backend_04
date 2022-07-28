1bb3684997455c9ffa759474107a008fd954a4c9
banssss <watermelonzv@gmail.com>
2022-07-28 21:42:39 +0900

    add: day19 class - N:M related APIs

---
+++ class/19-02-signup/src/apis/productsCategories/productsCategories.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductCategory } from './entities/productCategory.entity';
+import { ProductCategoriesResolver } from './productsCategories.resolver';
+import { ProductCategoriesService } from './productsCategories.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      ProductCategory, //
+    ]),
+  ],
+  providers: [
+    ProductCategoriesResolver, //
+    ProductCategoriesService,
+  ],
+})
+export class ProductCategoriesModule {}
---
+++ class/19-02-signup/src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardsModule } from './apis/boards/boards.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProductCategoriesModule } from './apis/productsCategories/productsCategories.module';
+import { ProductsModule } from './apis/products/products.module';
+import { UsersModule } from './apis/users/users.module';
+
+@Module({
+  imports: [
+    // dotenv module. (add @nestjs/config)
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BoardsModule,
+    ProductsModule,
+    ProductCategoriesModule,
+    UsersModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+})
+export class AppModule {}
