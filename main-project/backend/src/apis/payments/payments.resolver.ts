a9caad673256f45af43a05f65337e3444dfcf016
banssss <watermelonzv@gmail.com>
2022-08-08 21:18:56 +0900

    add: add DB transaction of paymentsForPoints on main-project

--- main-project/backend/src/apis/payments/payments.resolver.ts
+++ main-project/backend/src/apis/payments/payments.resolver.ts
@@ -113,4 +113,99 @@ export class PaymentsResolver {
       status: PAYMENT_STATUS_ENUM.CANCEL,
     });
   }
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => Payment)
+  async createPaymentForPoints(
+    @Args('impUid') impUid: string,
+    @Args({ name: 'paymentTotal', type: () => Int }) paymentTotal: number,
+    @Context() context: IContext,
+  ) {
+    /***   결제 검증 시작   ***/
+    // 1. 아임포트 억세스 토큰 발급받기
+    const impToken = await this.iamportsService.getImpAccessToken();
+    // 2. imp_uid로 아임포트 서버에서 결제 정보 조회
+    const getPaymentData = await this.iamportsService.getImpPaymentData({
+      access_token: impToken,
+      imp_uid: impUid,
+    });
+    // 조회한 결제 정보
+    const paymentData = getPaymentData.data.response;
+    // 3. 결제정보의 결제금액과 요청받은 결제금액 검증하기
+    if (paymentTotal !== paymentData.amount) {
+      throw new UnprocessableEntityException(
+        '결제정보의 위조/변조 시도가 발견되었습니다. 요청하신 정보를 저장할 수 없습니다.',
+      );
+    }
+    /***   결제 검증 종료   ***/
+
+    // 포인트 충전을 위한 결제 테이블 생성 - 결제를 생성한 유저와 연결하여 결제 내역 저장
+    const user = context.req.user;
+    return this.paymentsService.createForPoints({
+      impUid,
+      paymentTotal,
+      user,
+      status: PAYMENT_STATUS_ENUM.PAYMENT,
+    });
+  }
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => Payment)
+  async cancelPaymentForPoints(
+    @Args('impUid') impUid: string,
+    @Context() context: IContext,
+  ) {
+    /***   취소 검증 시작   ***/
+
+    // 1. 입력받은 impUid 값을 통해 payment table에서 data 검증하기.
+    await this.paymentsService.checkIsAbleToCancel({
+      impUid,
+      user: context.req.user,
+    });
+
+    // 2. 아임포트 억세스 토큰 발급받기
+    const impToken = await this.iamportsService.getImpAccessToken();
+
+    // 3. imp_uid로 아임포트 서버에서 결제 정보 조회
+    const getPaymentData = await this.iamportsService.getImpPaymentData({
+      access_token: impToken,
+      imp_uid: impUid,
+    });
+
+    /***   취소 검증 종료   ***/
+
+    // 조회한 결제 정보
+    const paymentData = getPaymentData.data.response;
+
+    /*** 부분환불을 위한 전처리(아임포트 Docs 참조) ***/
+    // 3-1. 조회한 결제정보로부터 imp_uid, amount(결제금액), cancel_amount(환불된 총 금액) 추출
+    const { imp_uid, amount, cancel_amount } = paymentData;
+    // 3-2. 환불 가능 금액 (= 결제금액 - 환불 된 총 금액) 계산
+    const cancelableAmount = amount - cancel_amount;
+    if (cancelableAmount <= 0) {
+      // 이미 전액 환불된 경우
+      return new UnprocessableEntityException('이미 전액 환불된 주문입니다.');
+    }
+    /*** 부분환불을 위한 전처리 끝 ***/
+
+    // 4. 아임포트 서버에서 결제 취소 api 요청
+    const canceledPayment = await this.iamportsService.cancelImpPaymentData({
+      access_token: impToken,
+      reason: '이곳에 환불사유를 입력해주세요',
+      imp_uid,
+      cancel_request_amount: amount, // 부분환불은 테스트모드에서 지원하지 않았습니다....!
+      cancelableAmount, // 부분환불은 테스트모드에서 지원하지 않았습니다....!
+    });
+
+    console.log(canceledPayment); // 취소내역 출력 테스트
+
+    // 결제 취소 테이블 생성 - 결제를 생성한 유저와 연결하여 결제 취소 내역 저장
+    const user = context.req.user;
+    return this.paymentsService.createForPoints({
+      impUid,
+      paymentTotal: -amount,
+      user,
+      status: PAYMENT_STATUS_ENUM.CANCEL,
+    });
+  }
 }
--- main-project/backend/src/apis/payments/payments.service.ts
+++ main-project/backend/src/apis/payments/payments.service.ts
@@ -1,11 +1,12 @@
 import {
   ConflictException,
+  HttpException,
   Injectable,
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PAYMENT_STATUS_ENUM } from 'src/commons/type/enums';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { Payment } from './entities/payment.entity';
 
@@ -17,6 +18,9 @@ export class PaymentsService {
 
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+
+    // 트랜잭션 적용을 위한 connection 선언
+    private readonly connection: Connection,
   ) {}
 
   async checkIsAbleToCancel({ impUid, user }) {
@@ -75,5 +79,78 @@ export class PaymentsService {
 
     // 5. 결제/취소 내역 결과 프론트엔드에 돌려주기
     return result;
+  }
+
+  // 포인트 결제내역 (payment 생성)
+  async createForPoints({ impUid, paymentTotal, user, status }) {
+    // ==== 데이터의 오염을 방지하기 위한 트랜잭션 적용 ==== //
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+
+    // ==================== transaction 시작 - SERIALIZABLE ==================== //
+    await queryRunner.startTransaction('SERIALIZABLE');
+    // ======================================================================== //
+
+    try {
+      // 1. 결제내역 테이블(Payment)에서, 기존 내역 확인
+      // const checkPayment = await this.paymentsRepository.findOne({ where: { impUid, status } });
+      const checkPayment = await queryRunner.manager.findOne(Payment, {
+        where: { impUid, status },
+        lock: { mode: 'pessimistic_write' }, // 이 트랜잭션에서 접근하는 동안, 다른 로직 접근 잠금.
+      });
+
+      // 1-1. 이미 저장된 결제/취소내역이라면, 오류메시지 전달
+      if (checkPayment) {
+        if (status === PAYMENT_STATUS_ENUM.PAYMENT)
+          throw new ConflictException('이미 저장된 결제내역입니다.');
+        if (status === PAYMENT_STATUS_ENUM.CANCEL)
+          throw new ConflictException('이미 저장된 취소내역입니다.');
+      }
+
+      // 2. 결제/취소 한 유저의 정보 찾아오기
+      // const buyer = await this.usersRepository.findOne({ where: { id: user.id } });
+      const buyer = await queryRunner.manager.findOne(User, {
+        where: { id: user.id },
+        lock: { mode: 'pessimistic_write' }, // 이 트랜잭션에서 접근하는 동안, 다른 로직 접근 잠금.
+      });
+
+      // ######## 2-1. 유저의 포인트 업데이트 ######## //
+      const updateUser = this.usersRepository.create({
+        ...buyer,
+        point: Number(buyer.point) + Number(paymentTotal),
+      });
+      await queryRunner.manager.save(updateUser);
+
+      // 3. 결제/취소 내역 테이블 (Payment) 에, 결제/취소 내역과 결제/취소 한 유저의 정보 연계하여 생성
+      const payment = this.paymentsRepository.create({
+        impUid,
+        paymentTotal,
+        user: updateUser,
+        status,
+      });
+
+      // 4. 생성한 정보 저장하기
+      // const result = await this.paymentsRepository.save(payment);
+      const result = await queryRunner.manager.save(payment); // queryRunner 를 통한 로직을 이용한 부분을 rollback 가능하다.
+
+      // ======================== commit - 성공 확정 선언 ========================= //
+      await queryRunner.commitTransaction();
+      // ======================================================================= //
+
+      // 5. 결제/취소 내역 결과 프론트엔드에 돌려주기
+      return result;
+    } catch (error) {
+      // ======================== rollback - 에러발생 시 ============================ //
+      await queryRunner.rollbackTransaction();
+      throw new HttpException(
+        error.response.message,
+        error.response.statusCode,
+      );
+      // ========================================================================= //
+    } finally {
+      // ====================== 무조건 실행되는 구간 : 연결 해제 ======================== //
+      await queryRunner.release();
+      // ========================================================================= //
+    }
   }
 }
