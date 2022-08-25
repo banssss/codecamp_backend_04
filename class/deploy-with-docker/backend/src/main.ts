74d912b71e70c7a7918240d662820474edca9ff8
banssss <watermelonzv@gmail.com>
2022-08-25 17:49:47 +0900

    update: ci/cd

--- class/deploy-with-docker/backend/src/main.ts
+++ class/deploy-with-docker/backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
 
 async function bootstrap() {
+  console.log('실행 테스트입니다. 1.6 버전 테스트에요! | backend/src/main.ts');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 검증기능 추가. class-validator
   app.useGlobalFilters(new HttpExceptionFilter()); // Exception Filter 등록하기
